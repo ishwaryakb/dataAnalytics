7ec06e66b150b1c8b75fa2f4a6bc97d8b7bb4acd
ishwarya <48169709+ishwaryakb@users.noreply.github.com>
2022-10-11 05:40:24 +0530

    Update instagramScraper.py

--- instagramScraper/instagramScraper.py
+++ instagramScraper/instagramScraper.py
@@ -2,7 +2,12 @@ import instaloader
 
 ig=instaloader.Instaloader()
 
+#enter the username 
 req_user=input("User name")
 
+#download profile picture of the given username
+ig.download_profile(req_user,profile_pic_only=True)
+
+#downloads all posts including metadata for the given username
 ig.download_profile(req_user,profile_pic=True)
 
